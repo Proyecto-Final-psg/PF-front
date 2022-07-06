05b1419c82eea6c769ab10c121b349e364351e06
Juano <juanocataldo@gmail.com>
2022-07-06 12:54:11 -0300

    Created navbar (Bootstrap5) standard

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,28 +1,29 @@
 import './carousel.css'
 import { useEffect } from 'react'
+import one from '../../assets/1.jpg'
 
 export function Carousel(){
    
 
-    return <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="..." class="d-block w-100" alt="..." />
+    return <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img src={one} className="d-block w-100" alt="..." />
       </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="..." />
+      {/* <div className="carousel-item">
+        <img src="..." className="d-block w-100" alt="..." />
       </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="..." />
-      </div>
+      <div className="carousel-item">
+        <img src="..." className="d-block w-100" alt="..." />
+      </div> */}
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
     </button>
   </div>
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,13 +4,15 @@ import { Carousel } from "../Carousel/Carousel";
 import { Info } from "../Info Panel/Info";
 import { Grid } from "../Comerce Grid/Grid";
 import { Footer } from "../Footer/Footer";
+import { NavBar } from "../NavBar/NavBar";
 
 
 
 const Home = () => {
   const state = useSelector(state => state)
   return (
-    <div className="cmp-hero">
+    <div className="cmp-hero">     
+      <NavBar /> 
       <Carousel />
       <Info />
       <Grid />
