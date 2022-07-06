48194bae9ccd66e716e12b63ace024f1bd395b12
Juano <juanocataldo@gmail.com>
2022-07-06 12:15:38 -0300

    created Info component and added to Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import "./Home.scss";
 import { useSelector } from "react-redux";
 import { Carousel } from "../Carousel/Carousel";
+import { Info } from "../Info Panel/Info";
 
 
 
@@ -9,6 +10,7 @@ const Home = () => {
   return (
     <div className="cmp-hero">
       <Carousel />
+      <Info />
       {console.log(state)}
     </div>
   );
