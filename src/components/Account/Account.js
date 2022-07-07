0479314c518c8df5533a64e5f0f278f18af5f774
Rodrigo <rodrigoperez0109@gmail.com>
2022-07-07 18:11:28 -0300

    se modifico dropdown nav

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -5,7 +5,6 @@ const Account = () => {
   return (
     <>
         <Profile />
-        sadsd
     </>
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,9 @@ import { useSelector } from "react-redux";
 import { Routes, Route, } from 'react-router-dom'
 import Nav from "../Nav/Nav";
 import PrincipalPage from "../PrincipalPage/PrincipalPage";
-<<<<<<< HEAD
-import Account from "../Account/Account";
-=======
 import { CardDetails } from "../CardDetails/CardDetails";
 import { EditCard } from "../EditCard/EditCard";
->>>>>>> 5de03566ec7ac326d5dc8bb0c0d2683623b7eecf
+import Account from "../Account/Account";
 
 const Home = () => {
   return (
@@ -16,12 +13,9 @@ const Home = () => {
       <Nav />
       <Routes>
         <Route path='/home' element={<PrincipalPage />} exact />
-<<<<<<< HEAD
-        <Route path='/account' element={<Account />} exact />
-=======
         <Route path='/products/:id' element={<CardDetails />} />
         <Route path='/products/edit/:id' element={<EditCard />} />
->>>>>>> 5de03566ec7ac326d5dc8bb0c0d2683623b7eecf
+        <Route path='/account' element={<Account />} exact />
       </Routes>
     </div>
   );
