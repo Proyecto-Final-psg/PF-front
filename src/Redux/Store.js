0330a6a6826296c4aed90c0057f60b3c0dd9b0b0
martin <barreiromartinj@gmail.com>
2022-07-08 00:22:46 -0300

    grred

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,12 +1,3 @@
-// import { createStore, applyMiddleware } from "redux";
-// import { reducer } from "./Reducers";
-// import Thunk from "redux-thunk";   //asyncronico!
-// // export const store = createStore(reducer)
-// export const store = createStore(
-//    reducer,
-//    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() , // esto es para q ande la herramienta devtool en browser
-//    applyMiddleware(Thunk)  //nos permite despacahr acciones asyncronas
-// )
 
 
 import { configureStore } from '@reduxjs/toolkit'
