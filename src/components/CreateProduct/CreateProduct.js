74545175c158e2d051cb48a0bc1ff6c3e492d3dd
Juano <juanocataldo@gmail.com>
2022-07-09 15:53:37 -0300

    added new visuals in Create Product

--- src/components/CreateProduct/CreateProduct.js
+++ src/components/CreateProduct/CreateProduct.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { createProduct } from '../../Redux/Actions';
+import imgCreate from '../../assets/empty.png'
 import './CreateProduct.scss'
 
 const CreateProduct = () => {
@@ -36,43 +37,58 @@ const CreateProduct = () => {
     }
 
     return (
-        <div className='create'>
+        <div>
             <h1>Create Product</h1>
+            <div className='create'>
+                <div className='form-create'>
+                    <form onSubmit={handleSubmit} className='create_form'>
+                        <label htmlFor='name'>
+                            <input type="text" value={createProd.name} placeholder='name' name='name' onChange={handleInputChange} />
+                        </label>
+                        <label htmlFor='stock'>
+                            <input type="number" value={createProd.stock} placeholder='stock' name='stock' onChange={handleInputChange} />
+                        </label>
+                        <label htmlFor='price'>
+                            <input type="number" value={createProd.price} placeholder='price' name='price' onChange={handleInputChange} />
+                        </label>
+                        <label htmlFor='type'>
+                            <select className='select' type='text' name='type' onChange={handleInputChange} >
+                                <option value="">type</option>
+                            </select>
+                        </label>
+                        <textarea name='description' value={createProd.description} type="text" placeholder="description" onChange={handleInputChange} />
+                        <label htmlFor='thc'>
+                            <input type="number" value={createProd.thc} placeholder='thc' name='thc' onChange={handleInputChange} />
+                        </label>
+                        <label htmlFor='cbd'>
+                            <input type="number" value={createProd.cbd} placeholder='cbd' name='cbd' onChange={handleInputChange} />
+                        </label>
+                        <label htmlFor='categories'>
+                            <select className='select' type='text' name='categories' onChange={handleInputChange} >
+                                <option value="">categories</option>
+                                {
+                                    state?.map( (c,i) => (
+                                        <option value={c.category} key={i}>{c.category}</option>
+                                    ) )
+                                }
+                            </select>
+                        </label>
+                        <button type='submit'>Crear</button>
+                    </form>
 
-            <form onSubmit={handleSubmit} className='create_form'>
-                <label htmlFor='name'>
-                    <input type="text" value={createProd.name} placeholder='name' name='name' onChange={handleInputChange} />
-                </label>
-                <label htmlFor='stock'>
-                    <input type="number" value={createProd.stock} placeholder='stock' name='stock' onChange={handleInputChange} />
-                </label>
-                <label htmlFor='price'>
-                    <input type="number" value={createProd.price} placeholder='price' name='price' onChange={handleInputChange} />
-                </label>
-                <label htmlFor='type'>
-                    <select className='select' type='text' name='type' onChange={handleInputChange} >
-                        <option value="">type</option>
-                    </select>
-                </label>
-                <textarea name='description' value={createProd.description} type="text" placeholder="description" onChange={handleInputChange} />
-                <label htmlFor='thc'>
-                    <input type="number" value={createProd.thc} placeholder='thc' name='thc' onChange={handleInputChange} />
-                </label>
-                <label htmlFor='cbd'>
-                    <input type="number" value={createProd.cbd} placeholder='cbd' name='cbd' onChange={handleInputChange} />
-                </label>
-                <label htmlFor='categories'>
-                    <select className='select' type='text' name='categories' onChange={handleInputChange} >
-                        <option value="">categories</option>
-                        {
-                            state?.map( (c,i) => (
-                                <option value={c.category} key={i}>{c.category}</option>
-                            ) )
-                        }
-                    </select>
-                </label>
-                <button type='submit'>Crear</button>
-            </form>
+                </div>
+
+                <div className='img-create'>
+                    <textarea value={createProd.name} id='name' />
+                    
+                    <textarea value={createProd.description} id='description' />
+
+                    <h4>${createProd.price}</h4>
+
+                </div>
+            </div>
+
+
         </div>
     )
 }
