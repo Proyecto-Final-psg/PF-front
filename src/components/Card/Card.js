44de0f8749ac0cab88da0013d890f26c34ab1b98
Rodrigo <rodrigoperez0109@gmail.com>
2022-07-10 15:25:25 -0300

    card stock

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,24 +4,28 @@ import { NavLink } from "react-router-dom";
 import { getProductById } from "../../Redux/Actions";
 import "./Card.scss";
 
-const Card = ({ name, id, description, img, price }) => {
+const Card = ({ name, id, description, img, price, stock }) => {
   return (
     <div className="card" style={{ backgroundImage: "" }}>
       <NavLink to={`/products/${id}`}>
-       
+
         <div className="card-details">
           <p className="text-title">{name}</p>
           <div className="card-img">
             <img className="card-img" src={img} alt={description} />
           </div>
           <p className="text-body">{description}</p>
-          <h2  className="card-price" > ${price}</h2>
+          <h2 className="card-price" > ${price}</h2>
         </div>
 
       </NavLink>
-      <button className="card-button">
-        <span className="material-symbols-outlined">add_shopping_cart</span>
-      </button>
+      <div className={`stock ${stock === 0 ? 'none' : (stock < 10 ? 'low' : '')}`}>{stock === 0 ? 'No stock' : (stock < 10 ? 'Low stock' : 'Stock')}</div>
+      {
+        stock > 0 &&
+        <button className="card-button">
+          <span className="material-symbols-outlined">add_shopping_cart</span>
+        </button>
+      }
     </div>
   );
 };
