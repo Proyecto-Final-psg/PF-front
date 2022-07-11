ae3df5f56d72165574f84d4d4cda37d4d54c2942
Rodrigo <rodrigoperez0109@gmail.com>
2022-07-11 18:00:29 -0300

    before merge

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,7 +9,7 @@ import { useSelector } from 'react-redux'
 
 const Nav = () => {
     const userRedux = useSelector(state => state.user)
-    const { user, isAuthenticated } = useAuth0()
+    const { user, isAuthenticated, loginWithRedirect } = useAuth0()
     return (
         <div>
             <Profile />
@@ -35,7 +35,7 @@ const Nav = () => {
                                 <a className="nav-link" href={() => false}>Categories</a>
                             </li>
                             {
-                                isAuthenticated &&
+                                isAuthenticated ?
                                 <li className="nav-item dropdown">
                                     <img src={user?.picture} alt={user.name} />
                                     <a className="nav-link dropdown-toggle" href={() => false} id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -46,6 +46,11 @@ const Nav = () => {
                                         <li><Logout /></li>
                                     </ul>
                                 </li>
+                                :
+                                <div className='sign'>
+                                    <button className='btn_in' onClick={() => { loginWithRedirect() }}>Sign in</button>
+                                    <button className='btn_up' onClick={() => { loginWithRedirect({ screen_hint: 'signup' }) }}>Sign up</button>
+                                </div>
                             }
                             <li className="nav-item carrito">
                                 <Link className="nav-link" to="#"><img src={Carrito} alt="alti1" /></Link>
