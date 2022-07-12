4fefcbe244e6a9dc1996d6cd00d2f615aa05751f
martin <barreiromartinj@gmail.com>
2022-07-12 15:52:32 -0300

    buttons admins remove edit

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -28,9 +28,11 @@ export function CardDetails() {
                     <button className='btn back' onClick={() => navigate(-1)}>
                         <span className="material-symbols-outlined">keyboard_backspace</span>
                     </button>
+                    <div className='container-buttons_edit_remove'>
+                        <NavLink className='button buton-edit' to={`/products/edit/${id}`}>Edit</NavLink>
+                        <button className='button '>Remove</button>
+                    </div>
 
-                    <NavLink className='button buton-edit' to={`/products/edit/${id}`}>Edit</NavLink>
-                    <button className='button '>Remove</button>
                     <h1>{product.name}</h1>
                     <hr />
                     {product && product.description ? <h5>{product.description}</h5> : <p>No description added</p>}
