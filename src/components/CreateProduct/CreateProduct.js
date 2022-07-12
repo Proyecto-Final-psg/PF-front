29dea5fcb53128aba20fa028e693b74e433f4fa5
martin <barreiromartinj@gmail.com>
2022-07-12 13:17:54 -0300

    log paginatro

--- src/components/CreateProduct/CreateProduct.js
+++ src/components/CreateProduct/CreateProduct.js
@@ -180,19 +180,15 @@ const CreateProduct = () => {
                 </div>
                 <div className='mockup-product'>
                     <div className='img-create'>
-
+                        <textarea className='img-create_title' defaultValue={createProd.name} id='name' />
+                        {console.log(createProd.img)}
                         {
                             createProd.img !== '' ?
-                            <img src={createProd.img} alt="alt4" />
-                            : null
+                                <img src={createProd.img} alt="alt4" />
+                                : null
                         }
-
-                        <textarea defaultValue={createProd.name} id='name' />
-
                         <textarea defaultValue={createProd.description} id='description' />
-
-                        <h4>${createProd.price}</h4>7
-
+                        <h4>${createProd.price}</h4>
 
                     </div>
                     <div className="buttons-categories">
--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -1,45 +1,44 @@
 import './Paginator.scss'
 
-export function Paginator({ postsPerPage, totalPosts, paginate, currentPage }){
+export function Paginator({ postsPerPage, totalPosts, paginate, currentPage }) {
     const pageNumbers = []
 
-
-    let maxPags = Math.ceil(totalPosts / postsPerPage )
+    let maxPags = Math.ceil(totalPosts / postsPerPage)
 
     for (let i = 1; i <= maxPags; i++) {
-        pageNumbers.push(i)        
-    }
-
-    function up(){
-        console.log('maxpag', maxPags)
-        if(currentPage < maxPags)
-            paginate(currentPage + 1) 
-
-        
-    }
-
-    function back(){
-        if(currentPage > 1 )
-            paginate(currentPage - 1) 
+        pageNumbers.push(i)
     }
     
-   
+    function up() {
+        // console.log('maxpag', maxPags)
+        if (currentPage < maxPags)
+            paginate(currentPage + 1)
+
+
+    }
+
+    function back() {
+        if (currentPage > 1)
+            paginate(currentPage - 1)
+    }
+
+
     return <nav className="pagination">
         {/* <ul> */}
         <button className='paginatorMove' onClick={back}>
-           <span className="material-symbols-outlined">arrow_back</span>
+            <span className="material-symbols-outlined">arrow_back</span>
         </button>
-            {pageNumbers.map( number => (
-                <div key={number.toString()}>
-            
+        {pageNumbers.map(number => (
+            <div key={number.toString()}>
+
                 <span key={number.toString()} className={currentPage === number ? 'active-pag' : 'inactive'}  >
-                    <button  className={currentPage === number ? 'active-pag' : 'pags inactive'} onClick={() => {paginate(number)}} key={number.toString()} >
-                        {number}   
+                    <button className={currentPage === number ? 'active-pag' : 'pags inactive'} onClick={() => { paginate(number) }} key={number.toString()} >
+                        {number}
                     </button>
-                </span>        
-        </div>))}
+                </span>
+            </div>))}
         <button className='paginatorMove' onClick={up} key='forward'>
-           <span className="material-symbols-outlined">arrow_forward</span>
+            <span className="material-symbols-outlined">arrow_forward</span>
         </button>
         {/* </ul> */}
     </nav>
