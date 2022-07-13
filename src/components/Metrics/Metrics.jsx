33c347a06f550c591352290265258326b0ee99b9
Juano <juanocataldo@gmail.com>
2022-07-13 13:24:26 -0300

    added another chart of sells

--- src/components/Metrics/Metrics.jsx
+++ src/components/Metrics/Metrics.jsx
@@ -2,11 +2,23 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Link } from 'react-router-dom'
 import { getAllProducts } from "../../Redux/Actions"
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title,  ArcElement, Tooltip, Legend } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
 import { Pie } from 'react-chartjs-2';
 import './Metrics.scss'
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+// ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+    ArcElement,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
 
 export function Metrics() {
 
@@ -17,7 +29,7 @@ export function Metrics() {
     let lowerThan50 = products.filter(p => p.stock < 50 && p.stock > 10)
 
 
-
+    //PIE CHART
     const data = {
         labels: ['No stock','Lower than 10', 'Lower than 50'],
         datasets: [
@@ -41,6 +53,60 @@ export function Metrics() {
         ],
     };
 
+    // YEAR CHART
+    const options = {
+        responsive: true,
+        interaction: {
+          mode: 'index' /*as const*/,
+          intersect: false,
+        },
+        stacked: false,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Sells of 2022',
+          },
+        },
+        scales: {
+          y: {
+            type: 'linear' /*as const*/,
+            display: true,
+            position: 'left' /*as const*/,
+          },
+          y1: {
+            type: 'linear' /*as const*/,
+            display: true,
+            position: 'right' /*as const*/,
+            grid: {
+              drawOnChartArea: false,
+            },
+          },
+        },
+      };
+      
+      const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+      
+      const data2 = {
+        labels,
+        datasets: [
+          {
+            label: 'Dataset 1',
+            data: products.map(p => p.stock),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            yAxisID: 'y',
+          },
+          {
+            label: 'Dataset 2',
+            data: products.map(p => p.name),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            yAxisID: 'y1',
+          },
+        ],
+      };
+      
+
 
     useEffect(() => {
         dispatch(getAllProducts())
@@ -48,8 +114,9 @@ export function Metrics() {
 
 
 
-    return <div className="container mb-5">
-        <h5 className="mt-5">Metrics</h5>
+    return <div className="container datas">
+        <h1 className="mt-5">Metrics</h1>
+        <hr />
         <div className="lower-10">
 
             <table class="table">
@@ -97,6 +164,10 @@ export function Metrics() {
             <div className="circular-chart">
                 <Pie data={data} />
             </div>
+        </div>
+        <br /><br />
+        <div className="sells-year">
+            <Line options={options} data={data2} />;
         </div>
     </div>
 }
