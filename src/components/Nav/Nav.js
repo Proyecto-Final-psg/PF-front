cecd9480523032f92b353629bcf4e9d455838a9f
Rodrigo <rodrigoperez0109@gmail.com>
2022-07-13 19:56:58 -0300

    first save bulma

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -13,7 +13,7 @@ const Nav = () => {
     var admin = userRedux.roll === "admin" || userRedux.roll === "super-admin"
     return (
         <div>
-            <Profile />
+            {/* <Profile />
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/home">
@@ -22,7 +22,7 @@ const Nav = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    {/* //////DEFINIR LINKS SEGUN ADMIN O USER /////// */}
+                    
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav">
                             {
@@ -58,6 +58,71 @@ const Nav = () => {
                                 <Link className="nav-link" to="/cart"><img src={Carrito} alt="alti1" /></Link>
                             </li>
                         </ul>
+                    </div>
+                </div>
+            </nav> */}
+            <nav className="navbar is-light has-shadow" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand logo_container">
+                    <Link className="brand" to="/home">
+                        <img src={Logo} />
+                    </Link>
+
+                    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" className="navbar-menu">
+                    {console.log(userRedux)}
+                    {
+                        admin &&
+                        <div className="navbar-start">
+                            <Link className="navbar-item" to="/products/create">
+                                Create product
+                            </Link>
+
+                            <Link className="navbar-item" to="/users">
+                                Users role
+                            </Link>
+                        </div>
+                    }
+
+                    <div className="navbar-end user_dropdown">
+                        {
+                            isAuthenticated ?
+                                <div className="navbar-item has-dropdown is-hoverable">
+                                    <a className="navbar-link avatar">
+                                        <img src={user.picture} alt='user' />
+                                        <p>{user.name}</p>
+                                    </a>
+
+                                    <div className="navbar-dropdown">
+                                        <Link className="navbar-item" to="/account">
+                                            Profile
+                                        </Link>
+                                        <hr className="navbar-divider" />
+                                        <a className="navbar-item">
+                                            <Logout />
+                                        </a>
+                                    </div>
+                                </div>
+                                :
+                                <div className="navbar-item">
+                                    <div className="buttons">
+                                        <button className="button is-secondary" onClick={() => { loginWithRedirect() }}>
+                                            Sign in
+                                        </button>
+                                        <button className="button is-primary" onClick={() => { loginWithRedirect({ screen_hint: 'signup' }) }}>
+                                            <strong>Sign up</strong>
+                                        </button>
+                                    </div>
+                                </div>
+                        }
+
+                        <Link className="navbar-item carrito" to="/cart"><img src={Carrito} alt="cart" /></Link>
+
                     </div>
                 </div>
             </nav>
