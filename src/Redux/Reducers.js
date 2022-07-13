946b4d3bf77fd220aed4132172c218e21dbb135d
GianfrancoGobbi <93214155+GianfrancoGobbi@users.noreply.github.com>
2022-07-13 01:27:18 -0300

    carrito local

--- src/Redux/Reducers.js
+++ src/Redux/Reducers.js
@@ -1,5 +1,5 @@
 
-import { GET_ALL_PRODUCTS, GET_ALL_ITEMS, ADD_TO_CART, GET_ALL_USERS, GET_PRODUCT_BY_ID, GET_ALL_CATEGORIES, REGISTER_USER, ADD_GUEST, EDIT_PRODUCT } from "./Constants"
+import { GET_ALL_PRODUCTS, GET_ALL_ITEMS, ADD_TO_CART, DELETE_TO_CART, UPDATE_TO_CART, GET_ALL_USERS, GET_PRODUCT_BY_ID, GET_ALL_CATEGORIES, REGISTER_USER, ADD_GUEST, EDIT_PRODUCT } from "./Constants"
 
 
 const initialState = {
@@ -49,14 +49,30 @@ export const reducer = (state = initialState, action) => {
                 users: action.payload
             }
         case ADD_TO_CART:
+            const cartfound = state.cart.find((p) => p.id === action.payload.id);
             return {
                 ...state,
-                cart: [...state.cart, action.payload]
+                cart: !cartfound ? [...state.cart, action.payload] : [...state.cart]
             }
+        case DELETE_TO_CART:
+            const filtercart = state.cart.filter((p) => p.id !== action.payload);
+            console.log(filtercart);
+            return {
+                ...state,
+                cart: filtercart
+            }
+        case UPDATE_TO_CART:
+            const cartfilter2 = state.cart.filter((p) => p.id !== action.payload.id);
+            return {
+                    ...state,
+                    cart: cartfilter2.concat(action.payload)
+            }
+
+
         case GET_ALL_ITEMS:
             return {
                 ...state,
-              }
+            }
 
 
         default:
--- src/components/CartItems/CartItems.js
+++ src/components/CartItems/CartItems.js
@@ -1,14 +1,33 @@
 import "./CartItems.scss";
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react';
+import { updateToCart, getAllItems, deleteToCart } from '../../Redux/Actions';
 
-const CartItems = ({ name, id, img, price, cant }) => {
+const CartItems = ({ name, id, price, cant }) => {
 
+  const allCartItems = useSelector(store => store.cart)
+  const dispatch = useDispatch()
+  const [contador, setContador] = useState(1)
+
+  useEffect(() => {
+    dispatch(getAllItems())
+  }, [contador])
+
+  const deleteItemToCart = (e) => {
+    dispatch(deleteToCart(id))
+  }
+  const updateCart = (event) => {
+    setContador(event.target.value);
+
+    dispatch(updateToCart(id, name, price, contador))
+  }
 
   return (
     <div className="cart-items">
-      <input className="cart-cant" type="number" placeholder={cant} ></input>
+      <input className="cart-cant" type="number" onChange={updateCart} value={cant} ></input>
       <span className="cart-span">{name}</span>
       <div className="cart-span">${price}</div>
-      <button className="item-delete">BORRAR</button>
+      <button onClick={deleteItemToCart} className="item-delete">BORRAR</button>
     </div>
   );
 };
