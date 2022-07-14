57e5a542a30aac37f71b64527d1898a77b3c892c
Juano <juanocataldo@gmail.com>
2022-07-14 16:50:39 -0300

    added user crud within admin panel

--- src/components/Metrics/UserManagement/UserManagement.jsx
+++ src/components/Metrics/UserManagement/UserManagement.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllUsers } from '../../../Redux/Actions'
+import User from '../../User/User'
+import Users from '../../Users/Users'
 import '../Metrics.scss'
 
 export function UserManagement(){
+
+    const usersprueba = useSelector(store => store.users)
+    const dispatch = useDispatch()
+    useEffect(() => {
+      dispatch(getAllUsers())
+             // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [usersprueba])
+
+
     return <div className="container datas">
     
     <h1 className="mt-5">User Management</h1>
@@ -8,8 +22,15 @@ export function UserManagement(){
 
     <div className="lower-10" style={{width:"100%"}}>
 
-    <div className="sells-year">
-      
+    <div className="">
+    {usersprueba && usersprueba.map(user =>
+          <User
+            key={user.user_email}
+            name={user.user_name}
+            roll={user.roll}
+            user_id={user.user_id}
+          />
+        )}
     </div>
   </div>
   </div>
