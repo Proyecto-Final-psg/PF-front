09f533e9373bea7107068705799ed846a6e0b4b5
Juano <juanocataldo@gmail.com>
2022-07-15 19:50:00 -0300

    showing customers with the totals of money in products

--- src/components/Metrics/TopCustomers/TopCustomers.jsx
+++ src/components/Metrics/TopCustomers/TopCustomers.jsx
@@ -1,19 +1,43 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { getAllOrders, getOrderItems } from "../../../Redux/Actions"
+import { getAllOrders, getAllUsers, getOrderDetails, getOrderItems, getUserById } from "../../../Redux/Actions"
+import { API_URL } from "../../../Redux/Constants"
 
 export function TopCustomers(){
 
   const dispatch = useDispatch()
-  const orders = useSelector(store => store.orderItems)
+  const orders = useSelector(store => store.order)
+  const users = useSelector(store => store.users)
+  const orderItems = useSelector(store => store.orderItems)
 
   useEffect(()=>{
+    dispatch(getOrderItems())
+    dispatch(getAllUsers())
     dispatch(getOrderItems())
   },[])
 
   useEffect(()=>{
+    
     console.log(orders)
   },[orders])
+
+  useEffect(()=>{
+    console.log(orderItems)
+  },[orderItems])
+
+  function getUserName(id){
+    const user = users.find(u => u.user_id == id)
+    return user.user_name
+  }
+
+  function getDetailsFrom(id){
+    
+    const ordersToSum = orderItems.filter(o => o.order == id)
+    const amountOfMoney = ordersToSum.reduce((a, b) => a + (b["price"] || 0), 0);
+    console.log('amount $',amountOfMoney)
+    return amountOfMoney
+  }
+
 
     return <div className="container datas">
     
@@ -24,13 +48,19 @@ export function TopCustomers(){
     <table class="table is-bordered is-narrow shadow">
         <thead>
           <tr>
-            <th><abbr title="ID">ID</abbr></th>
-            <th>Product</th>
-            <th><abbr title="Stock">Stock</abbr></th>
-            <th><abbr title="Action">Action</abbr></th>
+            <th><abbr title="ID">Name</abbr></th>
+            <th>Orders</th>
+            <th><abbr title="Stock">Totals</abbr></th>
           </tr>
         </thead>
         <tbody>
+          {orders && orders.map(o => {
+            return <tr>
+              <td>{getUserName(o.userUserId)}</td>
+              <td>{o.id}</td>
+              <td>${getDetailsFrom(o.id)}</td>
+            </tr>
+          })}
           </tbody>
           </table>
   </div>
