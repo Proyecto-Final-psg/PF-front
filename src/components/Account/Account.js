7ac44cce8450bc9b0481a782d1031cf92a3fff59
martin <barreiromartinj@gmail.com>
2022-07-16 03:55:12 -0300

    mercadoPago

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line 
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Profile from "../Profile/Profile";
 import "./Account.scss";
 import bag from "../../assets/bag.png";
 import { useAuth0 } from "@auth0/auth0-react";
--- src/components/PrincipalPage/PrincipalPage.js
+++ src/components/PrincipalPage/PrincipalPage.js
@@ -2,13 +2,12 @@ import './PrincipalPage.scss'
 import Grid from '../ComerceGrid/Grid'
 import Carousel from '../Carousel/Carousel'
 // import { useSelector } from 'react-redux'
-
 const PrincipalPage = () => {
     // const userRedux = useSelector(state => state.user)
 
     return (
         <div>
-        
+
             <Carousel classname="carousel" />
             <Grid />
         </div>
