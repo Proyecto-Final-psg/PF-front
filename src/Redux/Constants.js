9abbd03ca6bbdc8efd4400f62f1c969e8643384f
Juano <juanocataldo@gmail.com>
2022-07-17 11:16:19 -0300

    User block UI

--- src/Redux/Constants.js
+++ src/Redux/Constants.js
@@ -14,6 +14,7 @@ export const GET_ORDER_DETAILS = "GET_ORDER_DETAILS"
 export const GET_USER_ORDER = 'GET_USER_ORDER'
 export const GET_ORDER_ITEMS = 'GET_ORDER_ITEMS'
 export const CHANGE_ROLL = "CHANGE_ROLL"
+export const GET_AUTH0_USERS = 'GET_AUTH0_USERS'
 export const API_URL = 'https://desarrollo-back.herokuapp.com'
 // export const API_URL = 'http://localhost:8081'
 
