81c891b9973ae18aafe6f69881b3fc75e0935a7f
GianfrancoGobbi <93214155+GianfrancoGobbi@users.noreply.github.com>
2022-07-18 03:00:54 -0300

    Testimony-responsive

--- src/components/Testimony/Testimony.js
+++ src/components/Testimony/Testimony.js
@@ -8,9 +8,11 @@ const Testimony = () => {
       <h1 className="testimony-h1">TESTIMONIALS</h1>
       <div className="testimony">
         <div className="video-container">
-          <img src={Brand} alt="brand" className='video-brand' />
+        <img src={Brand} alt="brand" className='video-brand' />
           <label className="video-label">Elsa experience CBD</label>
+        
           <video className="video-html" controls >
+        
             <source src="https://cannabity.com/wp-content/uploads/2021/12/Francina-Colonques-Artritis-Articulaciones-Aceite-CBD-Cannabi-Cannabity-1.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
@@ -28,6 +30,22 @@ const Testimony = () => {
           <label className="video-label">Antrodesis experience</label>
           <video className="video-html" controls >
             <source src="https://cannabity.com/wp-content/uploads/2021/12/Ricardo-Soto-Aceite-CBD-Cannabi-Cannabity_1.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="video-container">
+          <img src={Brand} alt="brand" className='video-brand' />
+          <label className="video-label">Epilepsy CBD</label>
+          <video className="video-html" controls >
+            <source src="https://cannabity.com/wp-content/uploads/2021/03/Epilepsia-Carlos-Perez-Cannabity.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="video-container">
+          <img src={Brand} alt="brand" className='video-brand' />
+          <label className="video-label">Epilepsy CBD</label>
+          <video className="video-html" controls >
+            <source src="https://cannabity.com/wp-content/uploads/2022/03/Epilepsia-Leyre-Sanz-Mascota-Cancer-piel-Cardiopata-Prostatitis-Aceite-cbd-Cannabity.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
