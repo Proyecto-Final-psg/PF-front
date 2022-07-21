a246c07607928e7395edb13578fd7aff0250bbf2
Rodrigo <rodrigoperez0109@gmail.com>
2022-07-21 20:44:03 -0300

    scrollbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, HashRouter } from 'react-router-dom'
 import Landing from './components/Landing/Landing'
 import Home from './components/Home/Home';
+import './scss/global.scss'
 
 import 'bulma/css/bulma.min.css'
 export default function App() {
