9ba55241209d1796e412b6526e7f281497442785
martin <barreiromartinj@gmail.com>
2022-07-22 19:07:12 -0300

    deploy main

--- src/components/Metrics/Orders/OrderDetailed/OrderDetailed.jsx
+++ src/components/Metrics/Orders/OrderDetailed/OrderDetailed.jsx
@@ -2,13 +2,13 @@ import { useState } from "react"
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate, useParams } from "react-router-dom"
-import {  getOrdersByOrderId } from "../../../../Redux/Actions"
+import { getOrdersByOrderId } from "../../../../Redux/Actions"
 import ButtonBack from "../../../CreateProduct/ButtonBack/ButtonBack"
 import Modal from 'react-modal'
 import './OrderDetailed.scss'
 
 import { API_URL, ModalStyleOrders } from "../../../../Redux/Constants"
-
+import Loading from "../../../Loading/Loading"
 import swal from 'sweetalert'
 import LoadingImg from '../../../../assets/Loading.gif'
 
@@ -18,21 +18,35 @@ export function OrderDetailed() {
     const prodName = useSelector(store => store.product)
     const products = useSelector(store => store.products)
     const users = useSelector(store => store.users)
+    const [order, setOrder] = useState({})
+    // const [itemName, setItemName] = useState('')
     const [modal, setModal] = useState(false)
     const [orderStatus, setOrderStatus] = useState('')
     const [loading, setLoading] = useState(false)
 
     const dispatch = useDispatch()
+
     useEffect(() => {
+        // dispatch(getItemsOfOrder(id))
         dispatch(getOrdersByOrderId(id))
         // eslint-disable-next-line 
     }, [])
 
+
+
     useEffect(() => {
         setOrder(orderDetailed)
-    
-           console.log(orderDetailed)
+        // setItemName(prodName.name)
+        console.log(orderDetailed)
     }, [orderDetailed, prodName])
+
+    // function getProdName(prodId) {
+    //     if (prodId) {
+    //         let prod = products.find(p => parseInt(p.id) === parseInt(prodId))
+    //         if (prod)
+    //             return prod.name
+    //     }
+    // }
 
     function orderStatusState(e) {
         setOrderStatus(e.target.value)
@@ -45,6 +59,7 @@ export function OrderDetailed() {
     function changeOrderStatus() {
         setLoading(true)
         console.log('Actualizar orden con ', orderStatus);
+        // updateOrderStatus(id,orderStatus)
         fetch(`${API_URL}/update-order?id=${id}&status=${orderStatus}`, {
             method: "PUT"
         })
@@ -67,15 +82,22 @@ export function OrderDetailed() {
             )
     }
 
- 
+    // function matchIdWithUser(id) {
+    //     if (id) {
+    //         let user = users.find(u => parseInt(u.user_id) === parseInt(id))
+    //         console.log(user)
+    //         return user.user_name;
+    //     }
+    // }
+
     return <div className="container datas">
         <h1 className="mt-5">Order Detailed</h1>
         {/* <button onClick={() => navigate(-1)}>Back</button> */}
         {loading &&
-                <div className='loadingGif'>
-                    <h3>Loading</h3>
-                    < img className='cmp-CardDetails-loading-img' src={LoadingImg} alt="my-gif" />
-                </div>}
+            <div className='loadingGif'>
+                <h3>Loading</h3>
+                < img className='cmp-CardDetails-loading-img' src={LoadingImg} alt="my-gif" />
+            </div>}
         <ButtonBack button={''} />
 
 
@@ -96,7 +118,7 @@ export function OrderDetailed() {
                         <option value="canceled">Canceled</option>
                     </select>
                     <button style={{ width: "100px" }} onClick={changeOrderStatus} className="btn btn-success">Ok</button>
-                    <button  onClick={() => setModal(false)} className="btn btn-basic">Cancel</button>
+                    <button onClick={() => setModal(false)} className="btn btn-basic">Cancel</button>
                     {/* </form> */}
 
                 </div>
@@ -113,8 +135,8 @@ export function OrderDetailed() {
                         <td><abbr title="User name">User</abbr></td>
                         <td colSpan='3'>
                             <abbr title="Products">Products</abbr>
-                         
-                            </td>
+
+                        </td>
                     </tr>
                     <tr>
                         <td colSpan='3'></td>
@@ -124,8 +146,8 @@ export function OrderDetailed() {
                     </tr>
                 </thead>
                 <tbody>
-                    {orderDetailed && orderDetailed.orden  && <tr key={orderDetailed.orden.id}>
-                        <td style={{fontWeight:"bold"}}>{orderDetailed.orden.id}</td>
+                    {orderDetailed && orderDetailed.orden && <tr key={orderDetailed.orden.id}>
+                        <td style={{ fontWeight: "bold" }}>{orderDetailed.orden.id}</td>
                         <td>
                             {orderDetailed.orden.status} <button onClick={modOrderStatus} className="btn btn-sm btn-success">Change</button>
                         </td>
@@ -134,17 +156,14 @@ export function OrderDetailed() {
                             {orderDetailed.productos && orderDetailed.productos.length > 0 && orderDetailed.productos.map(i => {
                                 return <div key={i.name}>
                                     <img src={i.img} alt="" id='detail-prod-pic' />
-                                        <span style={{fontWeight:"bold"}}>{i.name} </span>
-                                        <span>[{i.quantity}u.] </span>
-                                        {/* <span>${i.priceOfSale}</span> */}
-                                    </div>
-                               
+                                    <span style={{ fontWeight: "bold" }}>{i.name} </span>
+                                    <span>[{i.quantity}u.] </span>
+                                    {/* <span>${i.priceOfSale}</span> */}
+                                </div>
+
                             })}
                         </td>
-
                     </tr>
-                  
-                        
                     }
                 </tbody>
             </table>
