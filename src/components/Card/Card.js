2581b50cdd67a45bdd3f779697feaa208d0a42a6
martin <barreiromartinj@gmail.com>
2022-07-26 01:12:30 -0300

    loading card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,11 +4,25 @@ import { addToCart } from "../../Redux/Actions"
 import { useDispatch } from 'react-redux'
 import party from "party-js";
 import Toastify from 'toastify-js'
-
+import LoadingImg from '../../assets/Loading.gif'
+import { useState, useEffect } from "react";
 const Card = ({ name, id, description, img, price, stock, widthProp, heightProp }) => {
   const dispatch = useDispatch()
-  function addItemToCart(e) {
+  const [loading, setLoading] = useState(true)
 
+  useEffect(() => {
+    // setTimeout(() => {
+    //   setLoading(false)
+    // }, 600)
+    // return () => {
+    //   setLoading(true)
+    // };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+
+
+  function addItemToCart(e) {
     //Efecto confeti
     party.confetti(e.target, {
       count: party.variation.range(20, 40),
@@ -40,9 +54,12 @@ const Card = ({ name, id, description, img, price, stock, widthProp, heightProp 
 
         <div className="card-details">
           <p className="text-title">{name}</p>
-          <div className="card-img">
+          {loading && <div className="card-img-loading">
+            <img className="card-img" src={LoadingImg} alt="LoadingImg" />
+          </div>}
+          {!loading && <div className="card-img">
             <img className="card-img" src={img} alt={description} />
-          </div>
+          </div>}
           <p className="text-body">{description}</p>
           <h2 className="card-price" > ${price}</h2>
         </div>
