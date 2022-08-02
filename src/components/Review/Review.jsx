4f1ba99a267e215927cd7210ec26d2f456b87766
Juano <juanocataldo@gmail.com>
2022-08-02 20:37:10 -0300

    J:REVIEWS UI

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -14,18 +14,42 @@ export function Review({ name, score, review }) {
             max={10}
             easing="cubic-bezier(.03,.98,.52,.99)"
             >
+                   <div class="card-rev">
+       <div class="content-rev">
+           <div class="front card_load">
+               <h3 class="title">
+               <div className="stars-review">
+                            {score && [...Array(score)].map((star, i) => {
+                                return (
+                                    <FaCannabis className='star-detail' key={i} style={{width:"20px"}}/>
+                                    )
+                                })}
+                        </div>
+               </h3>
+               <p class="subtitle">
+               <h2><b>{name}</b></h2>
+               </p>
+           </div>
 
-                <div className="cardreview">
-                        <h2><b>{name}</b></h2>
-                        <div className="stars">
+           <div class="back">
+               <p class="description">
+               <>{review}</>
+               </p>
+           </div>
+       </div>
+   </div>
+
+                {/* <div className="cardreview">
+                        <div className="stars-review">
                             {score && [...Array(score)].map((star, i) => {
                                 return (
                                     <FaCannabis className='star-detail' key={i} />
-                                )
-                            })}
+                                    )
+                                })}
                         </div>
+                                <h2><b>{name}</b></h2>
                         <p>{review}</p>
-                </div>
+                </div> */}
             </Hover>
         </div>
     )
