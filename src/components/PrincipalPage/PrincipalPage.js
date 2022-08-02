4cad96c9ee1a4e8a88d0bc0a2545e11ef1bb4e66
martin <barreiromartinj@gmail.com>
2022-08-02 14:35:05 -0300

    probando

--- src/components/PrincipalPage/PrincipalPage.js
+++ src/components/PrincipalPage/PrincipalPage.js
@@ -1,15 +1,22 @@
 import './PrincipalPage.scss'
 import Grid from '../ComerceGrid/Grid'
 import Carousel from '../Carousel/Carousel'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 const PrincipalPage = () => {
+    const [refresh, setRefresh] = useState(true)
+
     useEffect(() => {
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+
+        return () => {
+            setRefresh(false)
+        };
     }, [])
     return (
         <div className='principal'>
-            <div style={{height:"100px"}}>
-                <Carousel className="carousel" />
+            <div style={{ height: "100px" }}>
+                {refresh && <Carousel className="carousel" />}
+
             </div>
             <Grid />
         </div>
